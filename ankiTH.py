ab804c98883c367ff8f5fdbce4f494696340f7b5
paidev2539 <paidev2539@gmail.com>
2021-02-27 22:42:23 +0700

    add html

--- ankiTH.py
+++ ankiTH.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+import sys
+sys.dont_write_bytecode = True
+import rainbow_divider_lib as rdl
+
 
 def search_vocab(word, exactly_mode=False,
                  src="NECTEC Lexitron Dictionary EN-TH"):
@@ -32,7 +36,8 @@ def search_vocab(word, exactly_mode=False,
                 result_lst.append(tr_lst)
 
         for idx, text in enumerate(result_lst):
-            result_lst[idx][1] = text[1][:text[1].find(', Syn.')]
+            if text[1].find(', Syn.') != -1:
+                result_lst[idx][1] = text[1][:text[1].find(', Syn.')]
             # print(text)
         return result_lst
     else:
@@ -48,24 +53,38 @@ def read_txt(file_name):
     return vocab_lst
 
 
-def text_convert(meaning_lst):
+def text_convert(meaning_lst, vocab):
     text = ''
-    for meaning in meaning_lst:
+    rgb_lst = rdl.divider2str(len(meaning_lst))
+    for idx, meaning in enumerate(meaning_lst):
         # print(meaning[1])
-        text = text + meaning[0] + ' : ' + meaning[1] + '<br>'
+        # found_vocab = '<p style="color:rgb(255, 99, 71);">'+meaning[0]+'</p>'
+        if meaning[0] == vocab:
+            found_vocab = '<span style="color:rgb(102, 255, 153); font-size:30px">'+meaning[0]+'</span>'
+            found_meaning = '<span style="color:rgb(102, 255, 153); font-size:30px">'+meaning[1]+'</span>'
+        else:
+            found_vocab = '<span style="color:rgb'+ rgb_lst[idx] +'; font-size:15px">'+meaning[0]+'</span>'
+            found_meaning = '<span style="color:rgb'+ rgb_lst[idx] +'; font-size:15px">'+meaning[1]+'</span>'
+        new_text = found_vocab + ' : ' + found_meaning
+        text = text + new_text + '<br>'
     return text
 
 
 def main():
     vocab_lst = read_txt('vocab.txt')
-    file2 = open('output.txt', "w", encoding="utf8")
+    output = open('output.txt', "w", encoding="utf8")
+    fail_output = open('fail_output.txt', "w", encoding="utf8")
     for vocab in vocab_lst:
         meaning_lst = search_vocab(vocab)
         if meaning_lst is not None:
             # print(search_vocab(vocab))
-            meaning_text = text_convert(meaning_lst)
-            file2.write(vocab + ';' + meaning_text + '\n')
-    file2.close()
+            meaning_text = text_convert(meaning_lst, vocab)
+            vocab = '<span style="color:rgb(233, 253, 226); font-size:60px">'+vocab+'</span>'
+            output.write(vocab + '@' + meaning_text + '\n')
+        else:
+            fail_output.write(vocab + '\n')
+
+    output.close()
 
     # search_vocab('carrot')
 
